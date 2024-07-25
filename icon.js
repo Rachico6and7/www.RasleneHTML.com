7d4f9002c57f733f566f17595949b6f16a36352d
Rachico6and7 <raslene67@gmail.com>
2024-07-25 16:56:19 +0100

    Add files via upload

---
+++ icon.js
@@ -0,0 +1,7 @@
+console.log("webicon")
+function iconic(){
+    document.getElementById("icon").innerHTML = "iconic";
+}
+function icon(){
+    document.getElementById("icon").innerHTML = "icon";
+}
---
+++ java.js
@@ -0,0 +1,9 @@
+console.log('Hello everyone!')
+
+function doSomething(){
+    document.getElementById("test").innerHTML = "Helo";
+}
+
+function change(){
+    document.getElementById("test").innerHTML = "Hello!";
+}
